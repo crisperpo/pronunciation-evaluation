cbe7a6d4d8f71b198d5802b8d4734b4af05d97ba
crisperpo <crispe1586@gmail.com>
2024-03-12 19:42:58 +0100

    update types

--- spa/src/services/types.d.ts
+++ spa/src/services/types.d.ts
@@ -1,3 +1,14 @@
+export interface AudioTranscriptionInput {
+  text_to_record: string;
+  learner_recording: string;
+}
+
+export interface AudioEvaluationInput {
+  text_to_record: string;
+  learner_recording: string;
+  sr_transcript_of_learner_recording: string;
+}
+
 export interface TranscriptionPiece {
   text_to_record: string;
   learner_recording: string;
