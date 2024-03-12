e9fbf812d21fdcf20e201c320f41429c9227c06a
crisperpo <crispe1586@gmail.com>
2024-03-12 19:42:52 +0100

    send get params

--- spa/src/services/getAudioTranscript.ts
+++ spa/src/services/getAudioTranscript.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import type { TranscriptionPiece } from './types';
+import type { AudioTranscriptionInput, TranscriptionPiece } from './types';
 
-const getAudioTranscription = () => {
+const getAudioTranscription = ({ text_to_record, learner_recording }: AudioTranscriptionInput) => {
   axios
-    .get('http://localhost:3000/get-audio-transcription')
+    .get(`http://localhost:3000/get-audio-transcription?text_to_record=${text_to_record}&learner_recording=${learner_recording}`)
     .then((response: { data: Array<TranscriptionPiece> }) => response.data);
 };
 
